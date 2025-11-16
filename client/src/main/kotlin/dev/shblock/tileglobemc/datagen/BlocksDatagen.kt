c6a7f9272e2486ecc21aceb941e2ffb4970eee92
shBLOCK <shblock3@gmail.com>
2025-11-17 06:42:34 +0800

    Minecraft blocks datagen

---
+++ client/src/main/kotlin/dev/shblock/tileglobemc/datagen/BlocksDatagen.kt
@@ -0,0 +1,101 @@
+package dev.shblock.tileglobemc.datagen
+
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+import net.minecraft.core.HolderLookup
+import net.minecraft.core.registries.Registries
+import net.minecraft.data.CachedOutput
+import net.minecraft.data.DataProvider
+import net.minecraft.data.PackOutput
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.properties.BooleanProperty
+import net.minecraft.world.level.block.state.properties.EnumProperty
+import net.minecraft.world.level.block.state.properties.IntegerProperty
+import java.util.concurrent.CompletableFuture
+
+private val BlockState.id get() = Block.getId(this)
+
+class BlocksDatagen(
+    val packOutput: PackOutput,
+    val registries: CompletableFuture<HolderLookup.Provider>
+) : DataProvider {
+    override fun run(cachedOutput: CachedOutput) = registries.thenCompose { registries ->
+        CompletableFuture.allOf(
+            *registries.lookupOrThrow(Registries.BLOCK).listElements().map { entry ->
+                val resLoc = entry.key().location()
+                val block = entry.value()
+
+                val blockData = JsonObject()
+
+                blockData.addProperty("resource_location", resLoc.toString())
+
+                val possibleStates = block.stateDefinition.possibleStates.toMutableList()
+                    .apply { sortBy { it.id } }
+                val statePropertyCombinations = if (block.stateDefinition.properties.isNotEmpty()) {
+                    block.stateDefinition.properties.map { it.possibleValues.size }.reduce(Int::times)
+                } else 1
+                check(possibleStates.size == statePropertyCombinations) {
+                    "Number of possible states (${possibleStates.size}) doesn't match the number of state property combinations ($statePropertyCombinations)"
+                }
+                val idBase = possibleStates.first().id.also { blockData.addProperty("id_base", it) }
+                blockData.addProperty("total_states", possibleStates.size)
+                blockData.addProperty("default_state", block.defaultBlockState().id - idBase)
+
+                val blockstatePropertiesData = JsonArray().also { blockData.add("blockstate_properties", it) }
+                for (property in block.stateDefinition.properties) {
+                    val blockstatePropertyData = JsonObject().also { blockstatePropertiesData.add(it) }
+
+                    blockstatePropertyData.addProperty("name", property.name)
+
+                    when (property) {
+                        is BooleanProperty -> {
+                            blockstatePropertyData.addProperty("type", "boolean")
+                            check(property.possibleValues == listOf(true, false))
+                        }
+
+                        is IntegerProperty -> {
+                            blockstatePropertyData.addProperty("type", "integer")
+                            check(property.possibleValues.zipWithNext().all { (a, b) -> a < b }) {
+                                "IntegerProperty possible values are not sorted??"
+                            }
+                            blockstatePropertyData.addProperty("min", property.possibleValues.first())
+                            blockstatePropertyData.addProperty("max", property.possibleValues.last())
+                        }
+
+                        is EnumProperty -> {
+                            blockstatePropertyData.addProperty("type", "enum")
+                            blockstatePropertyData.add(
+                                "values",
+                                JsonArray().also { arr ->
+                                    property.possibleValues.forEach { arr.add(it.serializedName) }
+                                }
+                            )
+                        }
+                    }
+
+                    val idDiv =
+                        possibleStates.takeWhile { it.getValue(property) == property.possibleValues.first() }.size
+                    val idMod = idDiv * property.possibleValues.size
+
+                    blockstatePropertyData.addProperty("id_mod", idMod)
+                    blockstatePropertyData.addProperty("id_div", idDiv)
+
+                    // check validity
+                    possibleStates.forEach { check(it.getValue(property) == property.possibleValues[(it.id - idBase) % idMod / idDiv]) }
+                }
+
+                DataProvider.saveStable(
+                    cachedOutput,
+                    blockData,
+                    packOutput.outputFolder
+                        .resolve("blocks")
+                        .resolve(resLoc.namespace)
+                        .resolve("${resLoc.path}.json")
+                )
+            }.toArray { n -> arrayOfNulls<CompletableFuture<*>>(n) }
+        )
+    }
+
+    override fun getName() = "TileGlobeMC: Blocks"
+}
